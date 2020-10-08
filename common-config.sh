57bf693b51ef4c41b9d9de73abec8af7eefbc7cb
dil4devops <67610864+dil4devops@users.noreply.github.com>
2020-10-08 11:24:59 -0500

    Update common-config.sh

--- common-config.sh
+++ common-config.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+#common script for all the service configmaps
+# kubectl apply -f https://github.com/dil4devops/KafkaConnect/blob/master/aa-kafka.yaml
+
 kubectl apply -f aa-kafka.yaml
 kubectl apply -f aa-provider-pipeline-configmap.yaml
+kubectl apply -f aa-elasticsearch-client-configmap.yaml
+kubectl apply -f aa-file-router-configmap.yaml
+kubectl apply -f aa-provider-csv-producer-configmap.yaml
+kubectl apply -f aa-provider-enrollment-validator-configmap.yaml
+kubectl apply -f aa-provider-transformer-configmap.yaml
+kubectl apply -f aa-redshift-consumer-configmap.yaml
